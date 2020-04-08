990f152718e5e027e70b77adeb0999015f48a62d
Kim Korte <psaniko@gmail.com>
2020-04-08 15:09:24 +0200

    Add METIS

---
+++ metis/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/DependInfo.cmake
@@ -0,0 +1,88 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/b64.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/b64.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/blas.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/blas.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/csr.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/csr.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/error.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/error.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/evaluate.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/evaluate.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/fkvkselect.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/fkvkselect.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/fs.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/fs.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/getopt.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/getopt.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/gkregex.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/gkregex.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/graph.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/graph.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/htable.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/htable.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/io.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/io.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/itemsets.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/itemsets.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/mcore.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/mcore.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/memory.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/memory.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/omp.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/omp.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/pdb.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/pdb.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/pqueue.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/pqueue.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/random.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/random.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/rw.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/rw.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/seq.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/seq.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/sort.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/sort.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/string.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/string.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/timers.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/timers.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/tokenizer.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/tokenizer.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/GKlib/util.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/util.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/auxapi.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/auxapi.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/balance.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/balance.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/bucketsort.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/bucketsort.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/checkgraph.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/checkgraph.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/coarsen.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/coarsen.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/compress.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/compress.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/contig.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/contig.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/debug.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/debug.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/fm.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/fm.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/fortran.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/fortran.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/frename.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/frename.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/gklib.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/gklib.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/graph.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/graph.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/initpart.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/initpart.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/kmetis.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/kmetis.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/kwayfm.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/kwayfm.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/kwayrefine.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/kwayrefine.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/mcutil.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/mcutil.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/mesh.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/mesh.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/meshpart.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/meshpart.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/minconn.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/minconn.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/mincover.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/mincover.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/mmd.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/mmd.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/ometis.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/ometis.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/options.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/options.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/parmetis.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/parmetis.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/pmetis.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/pmetis.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/refine.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/refine.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/separator.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/separator.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/sfm.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/sfm.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/srefine.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/srefine.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/stat.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/stat.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/timing.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/timing.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/util.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/util.c.o"
+  "/Users/kim/Desktop/stu/metis-5.1.0/libmetis/wspace.c" "/Users/kim/Desktop/stu/metis-5.1.0/build/Darwin-x86_64/libmetis/CMakeFiles/metis.dir/wspace.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "metis_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../GKlib"
+  "../../include"
+  "../../libmetis/."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
