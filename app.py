9538be4ef28436844ae5f80dad4952799d4d5666
Kim Korte <psaniko@gmail.com>
2020-04-13 21:52:51 +0200

    more stuff

---
+++ app.py
@@ -0,0 +1,50 @@
+import csv
+import io
+from base64 import b64decode
+
+import networkx as nx
+from flask import Flask, request, jsonify
+from werkzeug.exceptions import BadRequest
+
+from solver import build_graph
+
+
+app = Flask(__name__, static_url_path='/static')
+
+
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/graph', methods=['POST'])
+def create_dot_file():
+    try:
+        num_partitions = request.json['num_partitions']
+    except KeyError as e:
+        raise BadRequest('Missing parameter "num_partitions"') from e
+
+    csv_encoded = request.json.get('csv_encoded')
+    csv_decoded = b64decode(csv_encoded).decode('utf-8')
+    students = csv.DictReader(io.StringIO(csv_decoded), delimiter=';')
+
+    cost, G = build_graph(
+        students=students,
+        num_partitions=num_partitions,
+        friendship_weight=request.json.get('friendship_weight', 30),
+        classmate_weight=request.json.get('classmate_weight', 3),
+        schoolmate_weight=request.json.get('schoolmate_weight', 1),
+        teacher_multiplier=request.json.get('teacher_multiplier', 100),
+        node_weights_to_ubvec=request.json.get('node_weights_to_ubvec', {
+            'total': 1.05,
+        }),
+    )
+
+    return jsonify({
+        'cost': cost,
+        'dot': str(nx.nx_pydot.to_pydot(G)),
+    })
+
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
--- solver.py
+++ solver.py
@@ -1,31 +1,155 @@
+import random
+import csv
+
 import networkx as nx
 import metis
-import csv
-from collections import Counter
 
-# constants
-RECOMMENDATIONS_TO_INT = {
-    'HS': 1,
-    'HS/RS': 2,
-    'RS': 3,
-    'RS/GY': 4,
-    'GY': 5,
-}
 
-NUM_PARITIONS = 3
+def build_graph_from_file(filename='students.csv', *args, **kwargs):
+    with open(filename) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=';')
+        return build_graph(reader, *args, **kwargs)
 
-FRIENDSHIP_WEIGHT = 30
-CLASSMATE_WEIGHT = 3
-SCHOOLMATE_WEIGHT = 1
 
-G = nx.Graph()
-G.graph['edge_weight_attr'] = 'weight'
-G.graph['node_weight_attr'] = ['always_one', 'gender_int', 'recommendation_int']
+def build_graph(
+    students,
+    num_partitions=3,
+    friendship_weight=30,
+    classmate_weight=3,
+    schoolmate_weight=1,
+    teacher_multiplier=100,
+    node_weights_to_ubvec=None,
+):
+    G = nx.Graph()
+    G.graph['edge_weight_attr'] = 'weight'
+    G.graph['node_weight_attr'] = list(node_weights_to_ubvec.keys())
 
-# helpers
-name_to_index = {}
-class_to_indices = {}
-school_to_indices = {}
+    # helpers
+    name_to_index = {}
+    class_to_indices = {}
+    school_to_indices = {}
+
+    i = 0
+    for row in students:
+        name = row['Name']
+        origin_class = row['School'] + ' ' + row['Class']
+        origin_school = row['School']
+        gender = row['Gender']
+        gender = row.get('Gender')
+
+        # gender_icon = '♂' if gender is 'm' else '♀'
+        G.add_node(
+            i,
+            label=name + "\n" + origin_class,
+            origin_school=origin_school,
+            origin_class=origin_class,
+            preferences=row['Preferences'],
+
+            # styling
+            fillcolor='#E0E0E0' if gender == 'm' else '#FFFFFF',
+            style='filled',
+            penwidth=3,
+
+            # node attributes
+            total=1,  # used to balance total number of students
+            gender=1 if gender == 'f' else 0,
+            **{
+                attribute: int(row[attribute.capitalize()])
+                for attribute in node_weights_to_ubvec.keys()
+                if attribute not in ('gender', 'total')
+            },
+        )
+
+        name_to_index[name] = i
+        if origin_class not in class_to_indices:
+            class_to_indices[origin_class] = set()
+        class_to_indices[origin_class].add(i)
+
+        if origin_school not in school_to_indices:
+            school_to_indices[origin_school] = set()
+        school_to_indices[origin_school].add(i)
+
+        i += 1
+
+    for i, node in G.nodes.items():
+        items = node['preferences'].split(',')
+        items = [item.strip() for item in items]
+
+        for item in items:
+            if not item:
+                continue
+
+            multiplier = 1
+            if item.startswith('!'):
+                multiplier = -1
+                item = item[1:]
+
+            if item.startswith('*'):
+                multiplier = multiplier * 100
+                item = item[1:]
+
+            (filter_attr, filter_value) = (None, None)
+            if item.startswith('['):
+                item_parts = item[1:].split(']')
+                filter_parts = item_parts[0].split('=')
+                assert len(filter_parts) == 2, 'Invalid filter: ' + item
+                (filter_attr, filter_value) = filter_parts
+                item = item_parts[1]
+
+            schoolmates = school_to_indices.get(item, None)
+            classmates = class_to_indices.get(item, None)
+            friend = name_to_index.get(item, None)
+
+            if schoolmates is not None:
+                # apply optional filter
+                if filter_attr:
+                    schoolmates = [
+                        i for i, node in G.nodes.items()
+                        if i in schoolmates and node[filter_attr] == filter_value
+                    ]
+
+                for schoolmate in schoolmates:
+                    if i == schoolmate:
+                        continue
+                    add_edge(G, i, schoolmate, weight=schoolmate_weight * multiplier)
+            elif classmates is not None:
+                # apply optional filter
+                if filter_attr:
+                    classmates = [
+                        i for i, node in G.nodes.items()
+                        if i in classmates and node[filter_attr] == filter_value
+                    ]
+
+                for classmate in classmates:
+                    if i == classmate:
+                        continue
+                    add_edge(G, i, classmate, weight=classmate_weight * multiplier)
+            elif friend is not None:
+                add_edge(G, i, friend, weight=friendship_weight * multiplier)
+            else:
+                print('Unhandled preference: ' + item)
+
+    # partition graph
+    (total_volume, parts) = metis.part_graph(
+        graph=G,
+        nparts=num_partitions,
+        tpwgts=[
+            tuple(
+                1 / num_partitions
+                for i in range(0, len(node_weights_to_ubvec))
+            )
+            for i in range(0, num_partitions)
+        ],
+        ubvec=list(node_weights_to_ubvec.values()),
+        objtype='cut',
+    )
+
+    # colors = ['red', 'blue', 'green']
+    colors = get_color_list(num_partitions)
+    for i, p in enumerate(parts):
+        G.node[i]['color'] = colors[p]
+
+    return total_volume, G
 
 
 # helper function to add visuals to edges
@@ -41,133 +165,36 @@ def add_edge(G, node1, node2, weight):
         node2,
         weight=weight,
         label=weight,
-        penwidth=min(FRIENDSHIP_WEIGHT, abs(weight)) / 10,
+        penwidth=min(30, abs(weight)) / 10,
         color='red' if weight < 0 else 'black',
     )
 
 
-# add students as nodes
-i = 0
-with open('students.csv') as csvfile:
-    reader = csv.DictReader(csvfile, delimiter=';')
-    for row in reader:
-        name = row['Vorname Mädchen'] + row['Vorname Junge'] + ' ' + row['Name']
-        origin_class = row['Abgebende Schule'] + ' ' + row['Klasse']
-        origin_school = row['Abgebende Schule']
-        gender = 'f' if row['Vorname Mädchen'] else 'm'
+def get_color_list(n):
+    colors = ['red', 'blue', 'green', 'magenta', 'yellow', 'teal']
 
-        # gender_icon = '♂' if gender is 'm' else '♀'
-        G.add_node(
-            i,
-            label=name + "\n" + origin_school + ' (' + row['SF-Emp-fehlung'] + ')',
-            gender=gender,
-            gender_int=1 if gender == 'f' else 0,
-            origin_school=origin_school,
-            origin_class=origin_class,
-            recommendation_int=RECOMMENDATIONS_TO_INT[row['SF-Emp-fehlung']],
-            preferences=row['Wunsch'],
-            fillcolor='#FFFFFF' if row['Vorname Mädchen'] else '#E0E0E0',
-            always_one=1,
-            style='filled',
-            penwidth=3,
-        )
-        name_to_index[name] = i
-        if origin_class not in class_to_indices:
-            class_to_indices[origin_class] = set()
-        class_to_indices[origin_class].add(i)
+    def r():
+        return random.randint(0, 255)
 
-        if origin_school not in school_to_indices:
-            school_to_indices[origin_school] = set()
-        school_to_indices[origin_school].add(i)
+    while n > len(colors):
+        colors.append('#%02X%02X%02X' % (r(), r(), r()))
+    return colors[:n]
 
-        i += 1
 
-# add preferences as edges
-for i, node in G.nodes.items():
-    items = node['preferences'].split(',')
-    items = [item.strip() for item in items]
+if __name__ == '__main__':
+    cost, G = build_graph_from_file(
+        filename='students.csv',
+        num_partitions=3,
+        friendship_weight=30,
+        classmate_weight=3,
+        schoolmate_weight=1,
+        teacher_multiplier=100,
+        node_weights_to_ubvec={
+            'total': 1.03,
+            'gender': 1.20,
+            'recommendation_int': 1.20,
+        },
+    )
 
-    for item in items:
-        if not item:
-            continue
-
-        multiplier = 1
-        if item.startswith('!'):
-            multiplier = -1
-            item = item[1:]
-
-        if item.startswith('*'):
-            multiplier = multiplier * 100
-            item = item[1:]
-
-        (filter_attr, filter_value) = (None, None)
-        if item.startswith('['):
-            item_parts = item[1:].split(']')
-            filter_parts = item_parts[0].split('=')
-            assert len(filter_parts) == 2, 'Invalid filter: ' + item
-            (filter_attr, filter_value) = filter_parts
-            item = item_parts[1]
-
-        schoolmates = school_to_indices.get(item, None)
-        classmates = class_to_indices.get(item, None)
-        friend = name_to_index.get(item, None)
-
-        if schoolmates is not None:
-            # apply optional filter
-            if filter_attr:
-                schoolmates = [
-                    i for i, node in G.nodes.items()
-                    if i in schoolmates and node[filter_attr] == filter_value
-                ]
-
-            for schoolmate in schoolmates:
-                if i == schoolmate:
-                    continue
-                add_edge(G, i, schoolmate, weight=SCHOOLMATE_WEIGHT * multiplier)
-        elif classmates is not None:
-            # apply optional filter
-            if filter_attr:
-                classmates = [
-                    i for i, node in G.nodes.items()
-                    if i in classmates and node[filter_attr] == filter_value
-                ]
-
-            for classmate in classmates:
-                if i == classmate:
-                    continue
-                add_edge(G, i, classmate, weight=CLASSMATE_WEIGHT * multiplier)
-        elif friend is not None:
-            add_edge(G, i, friend, weight=FRIENDSHIP_WEIGHT * multiplier)
-        else:
-            print('Unhandled preference: ' + item)
-
-# partition graph
-(total_volume, parts) = metis.part_graph(
-    graph=G,
-    nparts=NUM_PARITIONS,
-    tpwgts=[
-        (1 / NUM_PARITIONS, 1 / NUM_PARITIONS, 1 / NUM_PARITIONS) for i in range(0, NUM_PARITIONS)
-    ],
-    ubvec=[1.030, 1.200, 1.200],
-    objtype='cut',
-)
-
-# calculate statistics
-counter = Counter(parts)
-print('total cost: ', total_volume)
-print('group sizes: ', ','.join(str(count) for count in counter.values()))
-
-colors = ['red', 'blue', 'green']
-used_colors = set()
-for i, p in enumerate(parts):
-    G.node[i]['color'] = colors[p]
-    used_colors.add(colors[p])
-
-women_by_group = {used_color: 0 for used_color in used_colors}
-for i, node in G.nodes.items():
-    if node['gender'] == 'f':
-        women_by_group[node['color']] += 1
-print('women per group: ', ','.join(str(count) for count in women_by_group.values()))
-
-# write to DOT file
-nx.nx_pydot.write_dot(G, 'graph.dot')  # Requires pydot or pygraphviz
+    nx.nx_pydot.write_dot(G, 'graph.dot')
+    print('Cost: ', cost)
